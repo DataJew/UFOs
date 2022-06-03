d9112d763dac98329d30647b03e46b2b84599830
Justin Holt <86810022+DataJew@users.noreply.github.com>
2022-06-03 18:59:21 -0400

    Add files via upload

---
+++ static/js/practice.js
@@ -0,0 +1,50 @@
+// // Simple JavaScript console.log statement
+// function printHello() {
+//   console.log("Hello there!");
+// }
+
+// using only 1 line
+// printHello = () => "Hello there!";
+
+// If the code and output in your console is getting cluttered, type clear() and press Enter to clear the working area of your console.
+
+// "fat arrow": =>
+
+// // Takes two numbers and adds them
+// function addition(a, b) {
+//   return a + b;
+// }
+
+// console.log(addition(4, 5));
+
+// Converted to an arrow function
+//addition = (a, b) => a + b;
+
+// // Functions can call other functions
+// function doubleAddition(c, d) {
+//   var total = addition(c, d) * 2;
+//   return total;
+// }
+
+//doubleAddition = (c, d => addition(c, d) * 2;
+
+// To iterate through each name in JavaScript, we can create a for loop
+// let friends = ["Sarah", "Greg", "Cindy", "Jeff"];
+
+// function listLoop(userList) {
+//    for (var i = 0; i < userList.length; i++) {
+//      console.log(userList[i]);
+//    }
+// }
+
+// create a for loop to iterate through an array of vegetables
+var vegetables = ["Carrots", "Peas", "Lettuce", "Tomatoes"];
+	// build the for loop
+for (var i = 0; i < vegetables.length; i++) {
+    console.log("I love " + vegetables[i]);
+}
+
+// loop through numbers without using an array, instead using static value
+for (var i = 0; i < 5; i++) {
+   console.log("I am " + i);
+}
